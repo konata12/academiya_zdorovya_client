643f8f51599d4a9a0f6a45264a453c0abaaaea92
dima <87605146+konata12@users.noreply.github.com>
2025-07-12 02:38:29 +0300

    fixed DetailsListInput

--- app/utils/hooks/admin/detailsForm/useOrderedListHandleKeyDown.ts
+++ app/utils/hooks/admin/detailsForm/useOrderedListHandleKeyDown.ts
@@ -1,11 +1,11 @@
-import { ListFormData, OrderComponent, OrderSliceNameType } from "@/app/types/data/details.type";
+import { ListFormData, ListOrderComponent, OrderComponent, OrderSliceNameType } from "@/app/types/data/details.type";
 import { useDetailsFormSelectSlice } from "@/app/utils/hooks/admin/detailsForm/useDetailsFormSelectSlice";
 import { useAppDispatch } from "@/app/utils/redux/hooks";
 import { useCallback } from "react";
 
 interface OrderedListHandleKeyDownHookProps {
     index: number
-    componentData: OrderComponent
+    componentData: ListOrderComponent
     orderSliceName: OrderSliceNameType
 }
 
@@ -19,16 +19,24 @@ export function useOrderedListHandleKeyDown({
     const dispatch = useAppDispatch()
 
     const detailsComponent = structuredClone(componentData)
-    const listFormData = detailsComponent.componentData as ListFormData
+    const listFormData = detailsComponent.data
+    const listFormErrors = detailsComponent.error
 
     const handleKeyDown = useCallback((e: React.KeyboardEvent, i: number) => {
         if (e.key === 'Enter' && !e.shiftKey) {
             e.preventDefault();
 
+            // HANDLE OPTIONS
             const newOptions = [...listFormData.options];
             // Insert empty string at the next position
             newOptions.splice(i + 1, 0, '');
-            (detailsComponent.componentData as ListFormData).options = newOptions
+            detailsComponent.data.options = newOptions
+
+            // HANDLE ERRORS
+            const newErrors = [...listFormErrors.options];
+            // Insert empty string at the next position
+            newErrors.splice(i + 1, 0, { message: '' });
+            detailsComponent.error.options = newErrors
 
             // Properly typed setValue call
             dispatch(updateDetailsComponent({
@@ -40,10 +48,17 @@ export function useOrderedListHandleKeyDown({
         if (e.key === 'Backspace' && listFormData.options[i] === '' && i > 0) {
             e.preventDefault();
 
+            // HANDLE OPTIONS
             const newOptions = [...listFormData.options];
             // Remove the current option
             newOptions.splice(i, 1);
-            (detailsComponent.componentData as ListFormData).options = newOptions
+            detailsComponent.data.options = newOptions
+
+            // HANDLE ERRORS
+            const newErrors = [...listFormErrors.options];
+            // Insert empty string at the next position
+            newErrors.splice(i, 1);
+            detailsComponent.error.options = newErrors
 
             // Properly typed setValue call
             dispatch(updateDetailsComponent({
