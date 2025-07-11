c4be92cad426fbbe9d4692d104353a65fbc431da
dima <87605146+konata12@users.noreply.github.com>
2025-07-12 02:41:12 +0300

    fixed useOrderedForm

--- app/utils/hooks/admin/detailsForm/useOrderedForm.ts
+++ app/utils/hooks/admin/detailsForm/useOrderedForm.ts
@@ -1,10 +1,14 @@
-import { DetailsFormData, DetailsFormDataEnum, DetailsFormDataEnumType, DetailsFormDataType, ImageFormData, ListFormData, OrderComponent, OrderSliceNameType, ParagraphFormData, QuouteFormData, TitleFormData } from "@/app/types/data/details.type"
-import { useDetailsFormSelectSlice } from "@/app/utils/hooks/admin/detailsForm/useDetailsFormSelectSlice"
-import { useAppDispatch } from "@/app/utils/redux/hooks"
-import { DragEndEvent } from "@dnd-kit/core"
-import { arrayMove } from "@dnd-kit/sortable"
-import { useCallback } from "react"
-import { FieldArrayWithId } from "react-hook-form"
+import { arrayMove } from '@dnd-kit/sortable';
+import {
+    DetailsFormData,
+    DetailsFormDataEnum,
+    OrderComponent,
+    OrderSliceNameType,
+    } from '@/app/types/data/details.type';
+import { DragEndEvent } from '@dnd-kit/core';
+import { useAppDispatch } from '@/app/utils/redux/hooks';
+import { useCallback } from 'react';
+import { useDetailsFormSelectSlice } from '@/app/utils/hooks/admin/detailsForm/useDetailsFormSelectSlice';
 
 
 export function useOrderedForm(orderSliceName: OrderSliceNameType) {
@@ -22,25 +26,25 @@ export function useOrderedForm(orderSliceName: OrderSliceNameType) {
         }
 
         order.forEach((component) => {
-            switch (component.componentType) {
+            switch (component.type) {
                 case DetailsFormDataEnum.TITLES:
-                    data.titles.push(component.componentData as TitleFormData)
+                    data.titles.push(component.data)
                     break;
 
                 case DetailsFormDataEnum.PARAGRAPHS:
-                    data.paragraphs.push(component.componentData as ParagraphFormData)
+                    data.paragraphs.push(component.data)
                     break;
 
                 case DetailsFormDataEnum.QUOUTES:
-                    data.quoutes.push(component.componentData as QuouteFormData)
+                    data.quoutes.push(component.data)
                     break;
 
                 case DetailsFormDataEnum.LISTS:
-                    data.lists.push(component.componentData as ListFormData)
+                    data.lists.push(component.data)
                     break;
 
                 case DetailsFormDataEnum.IMAGES:
-                    data.images.push(component.componentData as ImageFormData)
+                    data.images.push(component.data)
                     break;
 
                 default:
@@ -59,8 +63,8 @@ export function useOrderedForm(orderSliceName: OrderSliceNameType) {
         if (active.id === over.id) return
 
         // Find the actual items in the order array
-        const activeItemIndex = order.findIndex(item => item.componentData.orderId === active.id)
-        const overItemIndex = order.findIndex(item => item.componentData.orderId === over.id)
+        const activeItemIndex = order.findIndex(item => item.data.orderId === active.id)
+        const overItemIndex = order.findIndex(item => item.data.orderId === over.id)
 
         if (activeItemIndex === -1 || overItemIndex === -1) return
 
@@ -71,27 +75,8 @@ export function useOrderedForm(orderSliceName: OrderSliceNameType) {
         dispatch(setDetailsStateOrder(newOrder))
     }, [])
 
-    // HELPER FUNCTIONS
-    const getFieldArrayIdByOrderId = useCallback(<T extends DetailsFormDataEnumType>(
-        orderedId: string,
-        fieldArray: FieldArrayWithId<DetailsFormData, T, "id">[]
-    ) => {
-        const field = fieldArray.find((field) => (field as any).orderId === orderedId)
-        return field?.id
-    }, [])
-
-    const getFieldArrayIndexByOrderId = useCallback(<T extends DetailsFormDataEnumType>(
-        orderedId: string,
-        fieldArray: FieldArrayWithId<DetailsFormData, T, "id">[]
-    ) => {
-        return fieldArray.findIndex((field) => (field as any).orderId === orderedId)
-    }, [])
-
     return {
         renderOrderedComponents,
-
         handleDragEnd,
-        getFieldArrayIdByOrderId,
-        getFieldArrayIndexByOrderId
     }
 }
