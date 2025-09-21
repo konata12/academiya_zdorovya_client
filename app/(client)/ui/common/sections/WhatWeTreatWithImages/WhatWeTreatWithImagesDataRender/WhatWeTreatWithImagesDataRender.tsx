ffea2f49a1eba4b4f0c7e86b004b6f8f6739c624
dima <87605146+konata12@users.noreply.github.com>
2025-09-21 22:49:48 +0300

    finished services page

--- app/(client)/ui/common/sections/WhatWeTreatWithImages/WhatWeTreatWithImagesDataRender/WhatWeTreatWithImagesDataRender.tsx
+++ app/(client)/ui/common/sections/WhatWeTreatWithImages/WhatWeTreatWithImagesDataRender/WhatWeTreatWithImagesDataRender.tsx
@@ -3,16 +3,51 @@
 import ButtonWithUnderlineAnimation from "@/app/common_ui/animated_components/ButtonWithUnderlineAnimation/ButtonWithUnderlineAnimation";
 import { AboutTreatment } from "@/app/types/data/about_treatment.type";
 import Image from "next/image";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import styles from "./WhatWeTreatWithImagesDataRender.module.scss";
 
 export function WhatWeTreatWithImagesDataRender({ data }: { data: AboutTreatment[] }) {
 	const [selected, setSelected] = useState(0);
+	const [buttonsCentered, setButtonsCentered] = useState(true);
+	const buttons = useRef<HTMLDivElement>(null);
+
+	useEffect(() => {
+		function preloadImages() {
+			data.forEach((treatment) => {
+				const img = new window.Image();
+				img.src = treatment.image; // this makes the browser download & cache
+			});
+		}
+
+		const observer = new ResizeObserver((entries) => {
+			if (buttons.current) {
+				const containerWidth = buttons.current.offsetWidth;
+				const children = Array.from(buttons.current.children);
+
+				const childNodesTotalWidth = children.reduce((sum, child) => {
+					const el = child as HTMLElement;
+					return sum + el.offsetWidth;
+				}, 0);
+
+				setButtonsCentered(containerWidth > childNodesTotalWidth);
+			}
+		});
+
+		preloadImages();
+		if (buttons.current) {
+			observer.observe(buttons.current);
+		}
+
+		return () => observer.disconnect();
+	}, []);
 
 	return (
 		<div className={styles.container}>
 			<div className={styles.buttonsContainer}>
-				<div className={styles.buttons}>
+				<div
+					className={`${styles.buttons} ${buttonsCentered ? "" : styles.left}`}
+					ref={buttons}
+				>
 					{data.map((treatmentTypes, i) => {
 						return (
 							<ButtonWithUnderlineAnimation
